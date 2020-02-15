9babe99da46b263be08b970920d73a261bec9f81
Ashkeyev Ali <ashkeyevli@gmail.com>
2020-02-15 10:14:44 +0600

    update1

--- Calculator1/app/src/main/java/com/example/calculator1/MainActivity.java
+++ Calculator1/app/src/main/java/com/example/calculator1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calculator1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Notification;
@@ -12,14 +13,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 private Button btn_zero,btn_one,btn_two,btn_three,btn_four,btn_five,btn_six,btn_seven,btn_eight,btn_nine;
 private Button btn_plus, btn_minus, btn_mult, btn_divide, btn_del,btn_c, btn_square,btn_root,btn_equal,btn_dot;
+private Button btn_percent, btn_sin,btn_cos,btn_tan,btn_cot, btn_ln,btn_log,btn_fact,btn_power,btn_powersqrt;
 private double number=0,number1=0;
 private TextView numberInput;
 private String delText;
 public char action;
 private String get;
+private double od;
+private int oi;
 
 
-private boolean check=true, checkNumber=true, checkAction=true, delete=true, checkzero=true;
+private boolean check=true, checkNumber=true, checkAction=true, delete=true, checkzero=true, notRoot=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,33 +50,34 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
         btn_root=findViewById(R.id.btn_root);
         btn_equal=findViewById(R.id.btn_equal);
         btn_dot=findViewById(R.id.btn_dot);
+        btn_percent=findViewById(R.id.btn_percent);
+        btn_sin=findViewById(R.id.btn_sin);
+        btn_cos=findViewById(R.id.btn_cos);
+        btn_tan=findViewById(R.id.btn_tan);
+        btn_cot=findViewById(R.id.btn_cot);
+        btn_ln=findViewById(R.id.btn_ln);
+        btn_log=findViewById(R.id.btn_log);
+        btn_fact=findViewById(R.id.btn_fact);
+        btn_power=findViewById(R.id.btn_power);
+        btn_powersqrt=findViewById(R.id.btn_powersqrt);
+        if(savedInstanceState!=null){
+            number=savedInstanceState.getDouble("number");
+            number1=savedInstanceState.getDouble("number1");
+            action=savedInstanceState.getChar("action");
+            checkAction=savedInstanceState.getBoolean("checkAction");
+            checkNumber=savedInstanceState.getBoolean("checkNumber");
+            check=savedInstanceState.getBoolean("check");
+            delete=savedInstanceState.getBoolean("delete");
+            numberInput.setText(savedInstanceState.getString("numberInput"));
+        }
 
 
     }
 
-  /*  public static String removezero(String str){
-        int i = 0;
-        while (i < str.length() && str.charAt(i) == '0')
-            i++;
-        StringBuffer sb = new StringBuffer(str);
-        sb.replace(0, i, "")
-        return sb.toString();
-    }*/
+
     public void onClick(View view) {
-        get=numberInput.getText().toString();
+
         try {
-
-          //  if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0'){
-           //     checkzero=false;
-
-          //  }
-          //  if(numberInput.length()>1)
-             //   checkzero=true;
-
-
-
-
-
 
 
            if (numberInput.getText().toString().equals("Infinity") || numberInput.getText().toString().equals("Wrong operation") || numberInput.getText().toString().equals("NaN")) {
@@ -84,18 +89,18 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                 switch (view.getId()) {
                     case R.id.btn_zero: {
 
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
 
-                        if(checkNumber && checkzero)
+                        if (checkNumber && checkzero)
                             numberInput.append("0");
                         break;
 
                     }
                     case R.id.btn_one: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -105,7 +110,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_two: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -115,7 +120,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_three: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -125,7 +130,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_four: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -135,7 +140,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_five: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -145,7 +150,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_six: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -155,7 +160,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_seven: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -165,7 +170,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_eight: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -175,7 +180,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         break;
                     }
                     case R.id.btn_nine: {
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
+                        if (numberInput.length() == 1 && numberInput.getText().toString().charAt(0) == '0') {
 
                             numberInput.setText("");
                         }
@@ -218,6 +223,18 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         numberInput.setText("");
                         action = '×';
                         break;
+                    }
+                    case R.id.btn_power: {
+                        check = true;
+                        checkNumber = true;
+                        checkAction = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number = Double.valueOf(numberInput.getText().toString());
+                        }
+                        numberInput.setText("");
+                        action='p';
+                        break;
+
                     }
                     case R.id.btn_divide: {
                         check = true;
@@ -269,6 +286,7 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                             numberInput.setText(String.valueOf(Math.sqrt(number)));
                         } else {
                             numberInput.setText("Wrong action");
+                            delete=false;
                         }
                         break;
                     }
@@ -277,10 +295,10 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                         check = false;
 
 
-
                         number1 = Double.valueOf(numberInput.getText().toString());
-                        if (number1 == 0&& action == '/') {
+                        if (number1 == 0 && action == '/') {
                             numberInput.setText("Cannot divide by 0");
+                            delete=false;
 
                         } else {
                             numberInput.setText(String.valueOf(action(number, number1)));
@@ -293,20 +311,152 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
 
                     case R.id.btn_dot: {
 
-                        if(numberInput.length()==1&&numberInput.getText().toString().charAt(0)=='0') {
 
-                            numberInput.setText("");
-                        }
 
                         if (check && checkNumber) {
 
-                            numberInput.setText(numberInput.getText() + "0.");
+
+                            numberInput.setText(numberInput.getText() + ".");
 
                             check = false;
                             checkzero = true;
                         }
                         break;
                     }
+                    case R.id.btn_percent: {
+                        check = true;
+                        checkNumber = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        numberInput.setText(String.valueOf(number1 / 100));
+                        break;
+                    }
+
+                    case R.id.btn_sin: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if (number1 == 30) {
+                            numberInput.setText("0.5");
+                        } else {
+                            numberInput.setText(String.valueOf(Math.sin(Math.toRadians(number1))));
+                        }
+                        break;
+                    }
+                    case R.id.btn_cos: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if (number1 == 60) {
+                            numberInput.setText("0.5");
+                        } else if (number1 == 90) {
+                            numberInput.setText("0");
+                        } else {
+                            numberInput.setText(String.valueOf(Math.cos(Math.toRadians(number1))));
+                        }
+                        break;
+                    }
+                    case R.id.btn_tan: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if (number1 == 90) {
+                            numberInput.setText("not defined");
+                            delete=false;
+                        } else {
+                            numberInput.setText(String.valueOf(Math.tan(Math.toRadians(number1))));
+                        }
+                        break;
+                    }
+                    case R.id.btn_cot: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if (number1 == 0) {
+                            numberInput.setText("not defined");
+                            delete=false;
+                        } else {
+                            numberInput.setText(String.valueOf(1 / (Math.tan(Math.toRadians(number1)))));
+                        }
+                        break;
+                    }
+                    case R.id.btn_ln: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if(number1<1){
+                            numberInput.setText("Not a number");
+                            delete=false;
+                        }
+                        else {
+
+                            numberInput.setText(String.valueOf(Math.log(number1)));
+                        }
+                        break;
+
+                    }
+                    case R.id.btn_log: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if(number1<1){
+                            numberInput.setText("Not a number");
+                            delete=false;
+                        }
+                        else {
+
+                            numberInput.setText(String.valueOf(Math.log10(number1)));
+                        }
+                        break;
+
+                    }
+                    case R.id.btn_fact: {
+                        checkNumber = true;
+                        check = true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number1 = Double.valueOf(numberInput.getText().toString());
+                        }
+                        int i,fact=1;
+                        for(i=1;i<=number1;i++){
+                            fact=fact*i;
+                        }
+                        numberInput.setText(String.valueOf(fact));
+                        break;
+
+                    }
+                    case R.id.btn_powersqrt:{
+                        checkNumber=true;
+                        check=true;
+                        checkAction=true;
+                        if (numberInput.getText().toString().length() != 0) {
+                            number = Double.valueOf(numberInput.getText().toString());
+                        }
+                        if (number >= 0) {
+                            numberInput.setText("");
+                            action='s';
+                            break;
+                        } else {
+                            numberInput.setText("Wrong action");
+                            delete=false;
+                        }
+                        break;
+
+                    }
+
+
                 }
 
         } catch (Exception e) {
@@ -325,10 +475,55 @@ private boolean check=true, checkNumber=true, checkAction=true, delete=true, che
                 return number * number1;
             case '/':
                 return number / number1;
+            case 'p':
+                return Math.pow(number,number1);
+            case 's': {
+                double dres = Math.pow(number, 1.0 / number1);
 
+                double ires = Math.round(dres);
+                double diff = Math.abs(dres - ires);
+                if (diff < Math.ulp(10.0)) {
+                    return ires;
+                }
+                else{
+                    return dres;
+
+
+                }
+            }
             default:
                 return 0;
+
+            }
+
         }
+
+    @Override
+      protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString("numberInput",numberInput.getText().toString());
+        outState.putDouble("number",number);
+        outState.putDouble("number1",number1);
+        outState.putBoolean("check",check);
+        outState.putBoolean("checkNumber",checkNumber);
+        outState.putBoolean("delete",delete);
+        outState.putBoolean("checkAction",checkAction);
+        outState.putChar("action",action);
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        savedInstanceState.getString("numberInput");
+        savedInstanceState.getString("number");
+        savedInstanceState.getString("number1");
+        savedInstanceState.getString("checkNumber");
+        savedInstanceState.getString("check");
+        savedInstanceState.getString("action");
+        savedInstanceState.getString("delete");
+        savedInstanceState.getString("checkAction");
+
+
+
     }
 
 
